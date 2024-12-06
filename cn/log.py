f398057605363bb99b010f158e74f0398746b09f
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2024-12-06 20:13:25 +0100

    Add art to logger

--- cn/log.py
+++ cn/log.py
@@ -1,3 +1,15 @@
 import sys
 
+import art
 from loguru import logger
+
+logger_format = (
+    "<green>{time:HH:mm:ss}</green> | "
+    "<level>{level: <8}</level> | "
+    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
+    "{message}"
+)
+logger.remove()
+logger.add(sys.stderr, format=logger_format)
+
+print(art.text2art("Capybara", font="big"))
